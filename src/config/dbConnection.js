d4c6b84f0b8d4c299c3452cf038a9a42a49e0741
MertSolgun <solgunmert@gmail.com>
2024-05-08 21:51:54 +0300

    feat:dbConnection

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,1 +1,10 @@
-const { mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => console.log("Db Connected"))
+  .catch(() => console.log("Db not connected"));
+
+module.exports = {
+  mongoose,
+};
