cdc48c7d49a7ed6f62e088d121e7af83fafd682d
MertSolgun <solgunmert@gmail.com>
2024-05-09 17:16:28 +0300

    feat:ordersOk

--- src/routes/order.js
+++ src/routes/order.js
@@ -7,9 +7,3 @@ router.route("/").get(order.list).post(order.create);
 router.route("/:id").get(order.read).put(order.update).delete(order.delete);
 
 module.exports = router;
-
-// {
-//     "username":"Mert",
-//     "password":"mert123",
-//     "address":"Ata mh 3490 sk"
-// }
